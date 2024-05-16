d5fd90e1dac8c43a2b443abeeb7d4b6edba6007c
ahsansiddiqui786 <ahsan125112006>
2024-05-16 15:28:36 -0700

    Added Via Github

---
+++ app.js
@@ -0,0 +1,689 @@
+// Question 1: Write an if statement that checks if a variable age is greater than or   equal to 18. If true, display "You are an adult,"
+// otherwise display "You are a minor."
+
+// var userAge = prompt("Enter Your Age")
+// console.log(userAge)
+
+// if (userAge >= 18) {
+//     alert("You're an Adult")
+// }
+// else {
+//     alert("You're a Minor")
+// }
+
+// Question 2: Ask the user to enter their name using prompt. If the name is "John," display "Hello, John!" Otherwise, display
+// "Hello, stranger!"
+
+// var userName = prompt("Enter Your Name")
+// console.log(userName)
+
+// if (userName === "John") {
+//     alert("Hello John!")
+// }
+// else {
+//     alert("Hello Stranger!")
+// }
+
+// Question 3: Calculate the area of a rectangle using the formula area = length * width. Ask the user to enter the length and width using prompt, and then display the result.
+
+// let length = prompt("Enter Your Room Length")
+// let width = prompt("Enter Your Room Width")
+// let calculation = length * width
+// alert("The area of the room, given its length " + length + " and width " + width + " " + " is " + calculation + " square feet.")
+
+// Question 4: Create a simple calculator. Ask the user to enter two numbers and an arithmetic operator (+, -, *, /). Perform the
+// corresponding operation and display the result.
+
+// let num = prompt("Enter Your First Number")
+// let num2 = prompt("Enter Your Second Number")
+// let addition = Number(num) + Number(num2)
+// alert("Dear User Here Is Your Sum " + (addition))
+
+// let num = prompt("Enter Your First Number")
+// let num2 = prompt("Enter Your Second Number")
+// let subtraction = num - num2
+// alert("Dear User Here Is Your Subtraction \n" + subtraction)
+
+// let num = prompt("Enter Your First Number")
+// let num2 = prompt("Enter Your Second Number")
+// let multiplication = num * num2
+// alert("Dear User Here Is Your Multiplication " + multiplication)
+
+// let num = prompt("Enter Your First Number")
+// let num2 = prompt("Enter Your Second Number")
+// let division = num / num2
+// alert("Dear User Here Is Your Divsion " + division)
+
+// Generate Percentage
+
+// let hundred = 100;
+// let adminAsk = prompt("Enter Your Class Name In Number")
+// let obtainMarks = prompt("Enter Your Obtain Marks")
+// let totalMarks = prompt("Enter Your Total Marks")
+// let percentage = obtainMarks / totalMarks * hundred
+// alert("Dear Student Your Class " + adminAsk + " Percentage Is " + (percentage.toFixed(5)) + "%")
+
+// Question 5: Ask the user to enter their age using prompt. If
+// age is between 13 and 19 (inclusive), display "You are a
+// teenager." Otherwise, display "You are not a teenager."
+
+// var age = prompt("Enter Your Age")
+// console.log(age)
+
+// if (age > 13 && age < 19) {
+//     console.log("You Are a Teenager")
+// }
+// else {
+//     console.log("You Are Not a Teenager")
+
+// }
+
+//Question 7: Ask the user to enter a number using prompt. Check if //the number is positive, negative, or zero, and display the
+//corresponding message.
+
+// let number = prompt("Enter Your Number")
+// console.log(number)
+
+// if (number > 0) {
+//     console.log("The Number Is Positive")
+// }
+// else if (number < 0) {
+//     console.log("The Number Is Negative")
+// }
+// else {
+//     console.log("The Number Is Zero")
+// }
+
+//Question 8: Ask the user to enter a grade (A, B, C, D, or F) using //prompt. Display a message indicating if they passed (grade
+//is A, B, C, or D) or failed (grade is F).
+
+// let askGrade = prompt("Enter Your Grade")
+// console.log(askGrade)
+
+// if (askGrade === "A" || askGrade === "A1" || askGrade === "A+" || askGrade === "A+1" || askGrade === "B" || askGrade === "C" || askGrade === "D" || askGrade === "E") {
+//     alert("Congratulation You're Passed")
+// }
+// else {
+//     alert("You're Failed")
+// }
+
+//Question 9: Ask the user to enter three numbers using prompt. Find the maximum and minimum of the three numbers and display the results.
+
+// let num1 = prompt("Enter a first number:");
+// let num2 = prompt("Enter a second number:");
+// let num3 = prompt("Enter a third number:");
+
+// let maximum = Math.max(num1, num2, num3);
+// let minimum = Math.min(num1, num2, num3);
+// console.log(maximum)
+// console.log(minimum)
+
+// Question 11: Ask the user to enter a number using prompt. Check if the number is even or odd, and display the corresponding
+// message.
+
+// let number = prompt("Enter A Number")
+// console.log(number)
+
+// if (number % 2 === 0) {
+//     console.log("Your Number Is Even")
+// }
+// else {
+//     console.log("Your Number Is Odd")
+
+// }
+
+// Question 13: Ask the user to enter their age using prompt. If the age is below 13, display "You are a child." If the age is
+// between 13 and 19 (inclusive), display "You are a teenager." If  the age is 20 or above, display "You are an adult."
+
+// var userAge = prompt("Enter Your Age")
+// console.log(userAge)
+
+// if (userAge < 13) {
+//     console.log("You are a Child")
+// } else if (userAge >= 13 && userAge <= 19) {
+//     console.log("You are a Teenager")
+// } else if (userAge >= 20) {
+//     console.log("You are an Adult")
+// }
+
+// Question 14: Ask the user to enter their name using prompt. Check if the name starts with the letter "A" (uppercase or lowercase) and display a message accordingly.
+
+// let myName = prompt("Enter Name")
+// let updatedName = myName.toUpperCase()
+// console.log(myName)
+
+// if(updatedName.charAt(0) === "A") {
+// console.log(updatedName)
+// }
+
+// let num_1 = 200;
+// let sum = (num_1 + 100) / 2
+// console.log(sum)
+
+// let user = "Ahsan Siddiqui"
+// let message = "Hello there,"
+// alert(message +" "+ user + "!")
+
+// let userName = prompt("Enter Your Name!")
+// alert("Hello " + userName.toLowerCase())
+
+// let user = "Ahsan Siddiqui"
+// let isFine = prompt("How Are You Doing!", user)
+// console.log(isFine)
+
+// let age = +prompt("What is your age?");
+// // let ifTrue = isPakistani.toLowerCase();
+
+// let age = prompt("Enter Your Age")
+
+// if (age == 10) {
+//   alert("matched");
+// } else {
+//   alert("non matched");
+// }
+
+// let userCountry = prompt("Enter User Country Name")
+// let upperCountry = userCountry.toUpperCase()
+// console.log(upperCountry.toUpperCase())
+
+// if (upperCountry === "OMAN") {
+//     console.log("Congratulation You're Selected")
+// }else if(upperCountry === "PAKISTAN") {
+// console.log("Congratulation You're Selected After 15 Days")
+// }else if(upperCountry === "DUBAI") {
+//     console.log("Congratulation You're Selected After 1.5 Month")
+//     }
+// else {
+//     console.log("Pending")
+// }
+
+// if (8 !== 5) {
+//     console.log("True")
+// }
+// else {
+//     console.log("False")
+// }
+
+// let studentAge = prompt("Enter Your Age")
+// console.log(studentAge)
+
+// if (studentAge >= 1 && studentAge <= 10) {
+//     console.log("you are under 10 you should ride on cycle ");
+// } else if (studentAge >= 11 && studentAge <= 20) {
+//     console.log("you are under 20 you are suppose to ride on bike!");
+// }
+// else {
+//     console.log("you can drive a car")
+// }
+
+
+// let age = prompt("enter a age?");
+
+// if (age < 20) {
+//     if (age < 10) {
+//         alert("you are under 10 you can eat lollipop !");
+//     } else {
+//         alert("you can't eat lollipop");
+//     }
+// } else {
+//     alert("you are greater than 20");
+// }
+
+// array
+
+// let array = ["Lamborghini" , "Ford" , "Volvo" , "Scorpio" , "Thar" , "Audi" , "Mercedes"]
+// console.log(array)
+
+// unshift() method add start of the array
+// shift() method remove start of the array without passes index
+// push() method add end of the array
+// pop() method remove last element of the array
+
+// array.unshift("Limousine")
+// console.log(array)
+
+// let fruits = [];
+// console.log(fruits)
+// fruits.push("Strawberry" , "Blueberry" , "Nesberry" , "Mango" , "Banana" , "Kivi")
+// console.log(fruits)
+// fruits.unshift("Papaya")
+// console.log(fruits)
+// fruits.pop()
+// console.log(fruits)
+// fruits.shift()
+// console.log(fruits)
+
+// unshift() method add start of the array
+
+// let sportsName = ["Hockey" , "Cricket" , "Football"]
+// console.log(sportsName)
+// sportsName.unshift("Table Tennis" , "Ludo")
+// console.log(sportsName)
+
+// shift() method remove start of the array without passes index
+
+// let carNames = ["Lamborgine" , "Ford" , "Volvo" , "Scorpio" , "Thar"]
+// console.log(carNames)
+// carNames.shift()
+// carNames.shift()
+// console.log(carNames)
+
+// push() method add end of the array
+
+// let studentNames = [];
+// studentNames.unshift("Ahsan 85%" , "Maaz 80%" , "Bazil 45%" , "Hazil 51%" , "Waqas 65%")
+// console.log(studentNames)
+
+// pop() method remove last element of the array
+
+// let bikeNames = [];
+// bikeNames.unshift("Suzuki 150", "Honda 125", "Suzuki 110", "CDI 70", "CD 70")
+// console.log(bikeNames)
+// bikeNames.pop()
+// bikeNames.pop()
+// console.log(bikeNames)
+
+// Math.round method kisi bhi no.ko 1 extra le leta hai agar wo . ke bad 5 ho ya 5 se ziada ho
+
+// let number = prompt("Enter Your Number")
+// let roundingNumber = Math.round(number)
+// console.log(roundingNumber)
+
+// ceil
+
+// let number = prompt("Enter Your Number")
+// let roundingNumber = Math.ceil(number)
+// console.log(roundingNumber)
+
+// floor
+
+// let number = prompt("Enter Your Number")
+// let roundingNumber = Math.floor(number)
+// console.log(roundingNumber)
+
+// random Number hai jo randomly 0. me randomly no. Generate Krta hai
+
+// let randomNumGenerate = Math.random()
+// console.log(randomNumGenerate)
+
+
+// Slice Method Again Review
+
+
+// let colorName = ['Green' , 'Blue' , 'Red' , 'Yellow' , 'Orange' , 'Pink' , 'Purple']
+// console.log(colorName)
+// requiredColor = colorName.slice(2 , 5)
+// console.log(requiredColor)
+
+// let carNames = ["Honda Every" , "Aqua" , "Swift" ,"Picanto" , "Ciciv" ,"Cultus"]
+// console.log(carNames)
+// favouriteCars = carNames.slice(4 , 6)
+// console.log(favouriteCars)
+
+// Splice Method Again Review
+
+// let companyName = ['HP' , 'Lenovo' , 'Samsung' , 'Dell' , 'Acer']
+// console.log(companyName)
+// companyName.splice(3 , 2 , 'Elite Book' , 'Note Book')
+// console.log(companyName)
+
+// let companyName = ["HP", "Dell","Lenovo","Acer"]
+// console.log(companyName)
+// companyName.splice(2 , 3 , "Apple" , "Asus" , "Acer")
+// console.log(companyName)
+
+
+// var carNames = ['Lamborghini' , 'Limousine' , 'Ford' , 'Volvo' , 'Fortuner']
+// console.log(carNames)
+// carNames.splice(1 , 3,"Scorpio" , "Thar")// index , Delete
+// console.log(carNames)
+
+// slice()
+
+// let colorName = ["Red" , "Green" , "Blue" , "Yellow" , "Black" ,"Orange"]
+// console.log(colorName)
+// favouriteColor = colorName.slice(3 , 5)
+// console.log(favouriteColor)
+
+// splice()
+
+// let colorName = ["Red" , "Green" , "Blue" , "Yellow" , "Black" ,"Orange"]
+// console.log(colorName)
+// colorName.splice(1 , 3 , "Indigo" , "Purple" , "White")
+// console.log(colorName)
+
+// Array elements delete using array operator
+
+// let x = [7 , 8 , 9 , 10]
+// delete x[1]
+// console.log(x)
+
+// let studentNames = ["Maaz" , "Bazil" , "Hazil" , "Umer" , "Rafay"]
+// console.log(studentNames)
+// delete studentNames[0]
+// console.log(studentNames)
+
+// let y =['Ahsan50%' , 'hamza48%' , 'Moin 45%' , 'dawood40%']
+// delete y[3]
+// console.log(y)
+
+// let text = "HELLO WORLD";
+// let letter = text.charAt(4);
+// console.log(letter)
+
+// let text = ("Elite Book")
+// console.log(text)
+// let copyLetter = text.slice(0 , 10)
+// console.log(copyLetter)
+
+// let text = ("Ahsan Siddiqui")
+// console.log(text)
+// let copyLetter = text.slice(6 , 14)
+// console.log(copyLetter)
+
+// let age = +prompt("Enter Your Age")
+// let condition = age > 13 && age < 20 ? "You're teenager" : "You're Not Teenager"
+// console.log(condition)
+
+// let studentName = prompt("Enter Your Name")
+// let studentName2 = prompt("Enter Your Name")
+
+// let ternaryOperator = studentName === "Bazil" && studentName2 === "Hazil" ? "You're Selected" : "You're Not Selected"
+
+// console.log(ternaryOperator)
+
+// let age = prompt("Enter Your Age")
+// let ternaryOperator = age > 12 && age < 20 ? "You're Child" : "You're Adult"
+
+// console.log(ternaryOperator)
+
+// let userNumber = prompt("Enter A Table Number")
+// let tableLength = prompt("Enter A table length")
+
+// for (let index = 1; index <= tableLength; index++) {
+//     let tableStatement = userNumber + "X" + index + "=" + (userNumber * index)
+//     console.log(tableStatement)
+// }
+
+// let tableNumber = 6;
+// let tableLength = 10;
+
+// for (let index = 1; index <= tableLength; index++) {
+//     let tableStatement = tableLength + "X" + index + "=" + (tableNumber * index)
+//     console.log(tableStatement)
+// }
+
+// var favTeam1 = prompt('Enter Your Favourite Team 1 ?');
+// var favTeam2 = prompt('Enter Your Favourite Team 2 ?');
+
+// if (favTeam1 === "Pakistan" && favTeam2 === "India") {
+//     console.log(favTeam1 + " Has Won")
+// }
+// else if (favTeam1 === "Newzealand" && favTeam2 === "Africa") {
+//     console.log(favTeam2 + " Has Won")
+// }
+// else {
+//     console.log(favTeam1 + " Has Loss")
+// }
+
+// var userQuery = prompt('Enter Your Query?');
+
+// if (userQuery === "Milk") {
+//     console.log("Milk is available")
+// }
+// else if (userQuery === "Yougert") {
+//     console.log("Yougert is available")
+// }
+// else if (userQuery === "Ensure") {
+//     console.log("Ensure is available")
+// } else if (userQuery === "Glucerna") {
+//     console.log("Glucerna is available")
+// }
+// else if (userQuery === "Lactogen") {
+//     console.log("Lactogen is Unavailable")
+// }
+// else {
+//     console.log("Please Go TO Mart")
+// }
+// setTimeeout() aik fuction or aik milliseconds leta hai
+
+// function greet() {
+//     console.log("Hello Good Morning")
+// }
+// setTimeout(greet, 5000);
+
+// function greet(name, byText) {
+//     console.log("Hello Good Morning" + " " + name +" "+ byText)
+// }
+// timeOut = setTimeout(greet, 500 , "Ahsan", "Take Care")
+// console.log(timeOut)
+
+// clearTimeout(timeOut)
+
+// function greet(name, byText) {
+//     console.log("Hello Good Morning" + " " + name + " " + byText)
+// }
+// intervalId = setInterval(greet, 1000 , "Ahsan", "Take Care");
+
+// clearInterval(intervalId)
+
+// function greet(name, byText) {
+//     console.log("Hello Good Morning" + " " + name + " " + byText)
+// }
+// timeOut = setTimeout(greet, 1000, "Maaz", "Take Care")
+// console.log(timeOut)
+
+// clearTimeout(timeOut)
+
+// function greet(name, byText) {
+//     console.log("Hello Good Morning" + " " + name + " " + byText)
+// }
+// ahsanCheck = setInterval(greet, 1000, "Maaz", "Allah Hafiz TC")
+// console.log(ahsanCheck)
+// clearInterval(ahsanCheck)
+
+// function displayTime() {
+//     let time = new Date();
+//     console.log(time);
+//     document.getElementById('time').innerHTML = time;
+// }
+// setInterval(displayTime, 1000)
+
+// for loop
+
+// let friends = ["Bazil" , "Hazil" , "Maaz" , "Rafay" , "Muavia"]
+
+// for (let index = 0; index < friends.length; index++) {
+//     console.log("Hello Friends, " + friends[index]);
+
+// }
+
+// while loop
+
+// let i = 0;
+
+// while (i < 4) {
+//     console.log(i)
+//     i++
+// }
+
+// let count = 1;
+
+// while(count <= 10) {
+// console.log("While Loop " + count)
+// count++
+// }
+
+// let i = 0;
+
+// while (i < 4) {
+//     console.log(`${i} is less than 4`)
+//     i++
+// }
+
+// let index = 1;
+
+// while (index <= 10) {
+//     console.log(`${index} is less than 10`)
+//     index++
+// }
+
+// let index = 1;
+
+// for (let index = 1; index < 10; index++) {
+//     console.log("For Loop " + index)
+
+// }
+
+// let index = 0;
+
+// while (index < 10) {
+//     console.log("While Loop " + index)
+//     index++
+// }
+
+// Do while loop
+
+// let index = 1;
+
+// do {
+//     console.log("Do While Loop " + index)
+//     index++
+// } while (index < 10);
+
+// function greet(name, message) {
+//     console.log("Dear Student" + " " + name + " " + message)
+// }
+// setTimeout(greet, 2000, "Ahsan Siddiqui", "Good Morning")
+
+// function comment(name, message) {
+//     console.log("Dear Consumer" + " " + name + " " + message)
+// }
+// functionClear = setInterval(comment, 1000, "Ahsan Siddiqui", "Your Tansaction is Successfully");
+// console.log(functionClear)
+
+// clearInterval(functionClear)
+
+// let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+// let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+// function date() {
+//     let getDate = new Date();
+//     let getDay = getDate.getDay();
+//     // console.log(days[getDay])
+//     let getMonth = getDate.getMonth();
+//     // console.log(months[getMonth])
+//     let getYear = getDate.getFullYear();
+//     // console.log(getYear)
+//     let hours = getDate.getHours();
+//     // console.log(hours)
+//     let minute = getDate.getMinutes();
+//     // console.log(minute)
+//     console.log(days[getDay] + " " + months[getMonth] + " " + getYear + " Hours:" + hours + " Minutes:" + minute)
+// }
+// setInterval(date, 2000)
+
+// var a = "This cannot be changed"
+// a = "I want to change this"
+// console.log(a)
+
+// let a = "This cannot be changed"
+// a = "I want to change this"
+// console.log(a)
+
+// const a = "This cannot be changed"
+// a = "I want to change this"
+// console.log(a)
+
+// const cups = 42;
+
+// switch (cups) {
+//     case 4:
+//         console.log("The Value Of Cups is 4")
+//         break
+//     case 41:
+//         console.log("The Value Of Cups is 41")
+//         break
+//     case 42:
+//         console.log("The Value Of Cups is 42")
+//         break
+//     case 43:
+//         console.log("The Value Of Cups is 43")
+//         break
+//     default:
+//         console.log("The Value Of Cups is None Of 4 , 41, 42 , 43")
+// }
+
+// let grade = "E";
+
+// switch (grade) {
+//     case "A":
+//         alert("You Got A Grade")
+//         break;
+//     case "B":
+//         alert("You Got B Grade")
+//         break;
+//     case "C":
+//         alert("You Got C Grade")
+//         break;
+//     case "D":
+//         alert("You Got D Grade")
+//         break;
+//     case "E":
+//         alert("You Got E Grade")
+//         break;
+
+//     default:
+//         alert("You Failed")
+//         break;
+// }
+
+// let userDish = "Biryani"
+
+// switch (userDish) {
+//     case "Biryani":
+//         console.log(userDish + " is Available")
+//         break;
+//     case "Singapori Rice":
+//         console.log("Singapori Rice is Available Please 5 minutes Wait")
+//         break;
+//     case "Pizza":
+//         alert("Pizza is Available Please 10 minutes Wait")
+//         break;
+//     case "Matka Fries":
+//         alert("Matka Fries is Available")
+//         break;
+//     case "Broast":
+//         alert("Broast is Available")
+//         break;
+//     case "Pasta":
+//         alert("Pasta is Unavailable")
+//         break;
+//     default:
+//         alert("Sorry, " + userDish + " is Out Of Stock")
+//         break;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
